16f1b22ca3d6cdbbb90cd45e6e997de5fbb3aee2
Anthony Poon <ypoon196@gmail.com>
2021-01-11 12:11:49 +0800

    Added comment and change to make things more clear

--- src/Controller/ToDoListController.php
+++ src/Controller/ToDoListController.php
@@ -8,25 +8,55 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-/**
- * Class ToDoListController
- * @package App\Controller
- * @Route("/todo-list")
- */
 class ToDoListController extends AbstractController {
     /**
-     * @Route("/")
-     * @param Request $request
-     * @param ToDoListService $db
-     * @return Response
+     * @Route("/todo-list", methods={"POST"})
+     *
+     * Controller is a interface between frontend and backend. When the frontend execute an ajax operation,
+     * the frontend will create a HTTP request and send it to the backend. A HTTP request includes, but not limited to
+     * the following:
+     *
+     * 1. URL
+     * 2. HTTP Method
+     * 3. Content-Type
+     * 4. Header
+     * 5. Content
+     *
+     * When the backend framework received the HTTP request, it will parse the request so that the controller can access
+     * the parameter send from the frontend. For example, consider the following request being send from the front end:
+     *
+     * Request URL: http://example.com/todo-list?id=1&name=abc
+     * Request Method: POST,
+     * Content-Type: application/json
+     * Content:
+     *
+     * {'item': 'lorem ipsum'}
+     *
+     * First, the controller will scan for all the controller method, and match the @Route annotation and know that addListItem
+     * should be called to handle this request.
+     *
+     * Second, the controller will parse '?id=1&name=abc' so that we can access 'id' with $request->query->get("id")
+     *
+     * Third, the content can be access by $request->getContent()
+     *
+     * Since the Content-Type is application/json. we need to use json_decode() to further parse it.
+     *
+     * Different programming language and framework might differ slightly on how things work, but the fundamental are
+     * pretty much the same.
      */
-    public function addListItem(Request $request, ToDoListService $db): Response{
-        $test = $request->query->get("test");
-        $items = $db->getItems();
+    public function addListItem(Request $request, ToDoListService $db): Response {
+        $content = json_decode($request->getContent(), true);
+        $db->addItem($content["item"]);
         return new JsonResponse([
-            "foo" => "bar",
-            "test" => $items
+            "status" => "success"
         ]);
     }
 
+    /**
+     * @Route("/todo-list", methods={"GET"})
+     */
+    public function getListItem(ToDoListService $db): Response {
+        $items = $db->getItems();
+        return new JsonResponse($items);
+    }
 }
--- src/Service/ToDoListService.php
+++ src/Service/ToDoListService.php
@@ -6,17 +6,35 @@ use PDO;
 class ToDoListService {
     private $db;
     public function __construct() {
+        // Connect to a database on start
         $this->db = new PDO(
-            "mysql:dbname=todo_list;host=anthony-poon.com",
+            "mysql:host=anthony-poon.com;dbname=todo_list",
             "todo_list",
             "CTmg52oOYF"
         );
     }
 
+    public function addItem(string $itemName) {
+        // Add an item to the database
+        $query = $this->db->prepare("
+            INSERT INTO
+                todo
+                (item_name)
+            VALUES
+                (:item_name)
+        ");
+        $query->bindValue(":item_name", $itemName);
+        $query->execute();
+    }
+
     public function getItems() {
-        $query = $this->db->query("SELECT * FROM todo");
+        // Retrieve all items from database
+        $query = $this->db->prepare("
+            SELECT
+                *
+            FROM todo
+        ");
         $query->execute();
         return $query->fetchAll(PDO::FETCH_ASSOC);
     }
-
 }
